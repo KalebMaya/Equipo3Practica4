6d440a11198f763a6f9e4ac9fe44b497434f737c
AlexisAng <alexiis1796.aa.aa@gmail.com>
2019-10-03 12:10:25 -0500

    Se agregaron clases

---
+++ equipo3practica4-movimientos/src/main/java/com/everis/equipo3practica4/model/repository/InventarioRepository.java
@@ -0,0 +1,5 @@
+package com.everis.equipo3practica4.model.repository;
+
+public interface InventarioRepository {
+
+}
---
+++ equipo3practica4-movimientos/src/main/java/com/everis/equipo3practica4/service/ProductoService.java
@@ -0,0 +1,5 @@
+package com.everis.equipo3practica4.service;
+
+public class ProductoService {
+
+}
