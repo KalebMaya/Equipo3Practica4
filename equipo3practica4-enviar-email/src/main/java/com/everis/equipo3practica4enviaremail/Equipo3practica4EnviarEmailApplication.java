a3e9f48235056e05b99758c34e990f25e472538f
Osmar Kaleb Maya Morales <vkaleb7@hotmail.com>
2019-10-03 09:36:54 -0500

    enviar notificaciones inicial

---
+++ equipo3practica4-enviar-email/src/main/java/com/everis/equipo3practica4enviaremail/Equipo3practica4EnviarEmailApplication.java
@@ -0,0 +1,13 @@
+package com.everis.equipo3practica4enviaremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo3practica4EnviarEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo3practica4EnviarEmailApplication.class, args);
+	}
+
+}
